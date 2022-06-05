8c7c94c32e9d64c15239d467f8591d5f3da1ce79
Wamuyu <michellewachira25@gmail.com>
2022-06-05 23:52:22 +0300

    I tried my best though JS is giving me so many issues

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "You have reached the end";
 
 document.querySelector("body").appendChild(h2);
+
+const p = document.createElement("p");
+package.textContent = "This color combination is my absolute favorite. Don't forget the twilight colors too. This is my first try in making a website and I am so glad!";
+
+document.querySelector("body").appendChild(p);
+
+const h3 = document.createElement("h3");
+h2.textContent = "JS is not for the weak";
